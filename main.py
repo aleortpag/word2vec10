d43eb0e270bd2b6edb3a52ec5502aafbd7fd309c
aleortpag <a.ortiz.pagador@gmail.com>
2023-06-09 20:00:28 +0200

    Entrenamiento del modelo

--- main.py
+++ main.py
@@ -3,52 +3,14 @@ import re
 import nltk
 from nltk import *
 from gensim.models import Word2Vec
+from gensim.models.keyedvectors import Word2VecKeyedVectors
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import numpy
 
-"""
-def tokenizar(fichero):
-    ps = PorterStemmer()
-    file = open(fichero, encoding='utf-8')
-    stop_words = open("Archivos/spanish.txt", encoding='utf-8')
-    stop_words = stop_words.read()
-    stop_words = unidecode(stop_words).split("\n")
-
-    lineas = unidecode(file.read())
-    sin_Numeros = re.sub("\d+", "", lineas)
-
-    palabras = re.split('[, .()#&%+:;\n"]', sin_Numeros)
-    lista = []
-    for i in palabras:
-        i = i.lower()
-        if (len(i) > 0):
-            if (i not in stop_words and i != "[" and i != "]" and i != "[]"):
-                lista.append(ps.stem(i, True))
-    return lista
-
-
-def contar_palabras(lista):
-    frecuencia = {}
-    for palabra in lista:
-        frecuencia[palabra] = frecuencia.get(palabra, 0)+1
-    lista_tuplas = [(palabra, contador)
-                    for palabra, contador in frecuencia.items()]
-    return sorted(lista_tuplas, reverse=True, key=lambda x: x[1])
-
-
-def eliminar_menores_de(lista, umbral):
-    res = []
-    for i in lista:
-        if i[1] > umbral:
-            res.append(i)
-    return res
-
-
-def elimina_pairs(lista_tuplas):
-    res = []
-    for i in lista_tuplas:
-        res.append(i[0])
-    return res"""
 
 LISTA_NO_ALFANUMERICOS = ["!", "@", "#", "$", "%", "&", "*", "(", ")", "-", "", "+", "=", "{", "}", "[", "]", "|", "'\'", ";", ":", ",", ".", "<", ">", "/",
                           "?", "¡", "¿", "¨", "´", "¨", "~", "¬", "`", "^", "¦", "ª", "º", "«", "»", "©", "®", "™", "§", "±", "µ", "÷", "×", "``", "\d"]
@@ -81,7 +43,7 @@ def tokenizar(archivo):
     return res
 
 
-def representa(vectores, similares):
+def representar(vectores, similares):
     # Reducción de dimensionalidad a 2 dimensiones
     pca = PCA(n_components=2)
     vectors_2d = pca.fit_transform(vectores.vectors)
@@ -105,21 +67,56 @@ def representa(vectores, similares):
     plt.show()
 
 
+def embeddings(corpus, vectores):
+    corpus_embeddings = []
+    for c in corpus:
+        # Obtener el embedding de cada palabra en la frase y promediarlos
+        embeddings = [vectores[word] for word in c if word in vectores]
+        if embeddings:
+            sentence_embedding = numpy.mean(embeddings, axis=0)
+            corpus_embeddings.append(sentence_embedding)
+    return corpus_embeddings
+
+
 if __name__ == "__main__":
-    corpus = tokenizar("archivos/texto1_corpus.txt")
+    # Corpus
+    corpus1 = tokenizar("archivos/texto1_corpus.txt")
+    corpus2 = tokenizar("archivos/texto2_corpus.txt")
+    corpus3 = tokenizar("archivos/texto3_corpus.txt")
+    corpus4 = tokenizar("archivos/texto4_corpus.txt")
+
+    corpus_total = corpus1 + corpus2 + corpus3 + corpus4
 
     # Entrenar el modelo Word2Vec
-    model = Word2Vec(sentences=corpus, vector_size=100,
-                     window=5, min_count=1, workers=4)
+    model1 = Word2Vec(sentences=corpus1, vector_size=100, window=5, min_count=1, workers=4)
+    model2 = Word2Vec(sentences=corpus2, vector_size=100, window=5, min_count=1, workers=4)
+    model3 = Word2Vec(sentences=corpus3, vector_size=100, window=5, min_count=1, workers=4)
+    model4 = Word2Vec(sentences=corpus4, vector_size=100, window=5, min_count=1, workers=4)
 
     # Obtener vectores de palabras
-    word_vectors = model.wv
+    word_vectors1 = model1.wv
+    word_vectors2 = model2.wv
+    word_vectors3 = model3.wv
+    word_vectors4 = model4.wv
 
-    # Realizar operaciones con los vectores
-    similar_words = word_vectors.similar_by_vector(
-        word_vectors.vectors.mean(axis=0), topn=15)
-    
-    representa(word_vectors, similar_words)
+    word_vectors_total = Word2VecKeyedVectors(vector_size=100)
+    word_vectors_total.add_vectors(word_vectors1.index_to_key, word_vectors1.vectors)
+    word_vectors_total.add_vectors(word_vectors2.index_to_key, word_vectors2.vectors)
+    word_vectors_total.add_vectors(word_vectors3.index_to_key, word_vectors3.vectors)
+    word_vectors_total.add_vectors(word_vectors4.index_to_key, word_vectors4.vectors)
 
-    # similarity = word_vectors.similarity("cero", "de")
-    print(similar_words)
+    # Dividir los datos en conjuntos de entrenamiento y prueba
+    tematicas = ['matematicas'] * len(corpus1) + ['salud'] * len(corpus2) + ['lengua'] * len(corpus3) + ['filosofia'] * len(corpus4)
+    total_embeddings = embeddings(corpus_total, word_vectors1)
+    X_train, X_test, y_train, y_test = train_test_split(total_embeddings, tematicas, test_size=0.6, random_state=42)
+
+    # Entrenar un modelo de clasificación
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    # Realizar predicciones en el conjunto de prueba
+    y_pred = model.predict(X_test)
+
+    # Evaluar el rendimiento del modelo
+    print(classification_report(y_test, y_pred))
+
